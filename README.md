6dbfa589fe8efde986202c703f1b235f0884e492
Mikhail Yurasov <me@yurasov.me>
2015-06-15 23:05:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LeanCI Manager
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Main gulpfile, see tasks in dev/gulp/tasks
+ * @copyright 2015 Mikhail Yurasov <me@yurasov.me>
+ */
+
+var requireDir = require('require-dir');
+
+// additional tasks
+requireDir('dev/gulp/tasks');
