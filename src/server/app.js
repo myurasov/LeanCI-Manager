d7b3572ce5ae65a8ec7827868731f42f5b402e3f
Mikhail Yurasov <me@yurasov.me>
2015-06-17 03:11:04 -0700

    Read local settings

--- src/server/app.js
+++ src/server/app.js
@@ -7,7 +7,6 @@
 
 var express = require('express');
 var fs = require('fs');
-var _ = require('lodash');
 
 // create app
 var app = express();
@@ -18,6 +17,13 @@ app.set('env', fs.readFileSync(__dirname + '/../../environment').toString().trim
 // configure app
 require('./config')(app);
 require('./config.' + app.get('env'))(app);
+
+// read local settings (unique for the current installation)
+if (fs.existsSync(app.get('paths.root') + '/settings.json')) {
+  app.set('local_settings', require(app.get('paths.root') + '/settings.json'));
+} else {
+  app.set('local_settings', {});
+}
 
 // listen
 var server = app.listen(app.get('port'), function () {
