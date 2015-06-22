8f0ee1f19bfc459caca4beb322b2cc13f46a0570
Mikhail Yurasov <me@yurasov.me>
2015-06-22 03:40:57 -0700

    Show alerts

--- src/client/js/controllers/SetupController.js
+++ src/client/js/controllers/SetupController.js
@@ -4,23 +4,41 @@
 
 'use strict';
 
-app.controller('SetupController', function ($scope, $http) {
+app.controller('SetupController', function ($scope, $http, $controller) {
 
-  // # of async processes working
-  $scope.working = 0;
+  // alerts
+  $controller('AlertsTrait', {$scope: $scope});
 
-  // user input
-  $scope.input = {
-    user: {
-      email: 'me@yurasov.me',
-      password: 'passw123'
-    },
-    passwordConfirmation: 'passw123'
-  };
+  initScope();
 
+  /**
+   * Initialize scope
+   */
+  function initScope() {
+    // # of async processes working
+    $scope.working = 0;
+
+    // user input
+    $scope.input = {
+      user: {
+        email: 'me@yurasov.me',
+        password: 'passw123'
+      },
+      passwordConfirmation: 'passw123'
+    };
+  }
+
+  /**
+   * Submit
+   */
   $scope.submit = function () {
     $scope.working++;
     $http.post('http://localhost:12345/api/users',{})
+      .then(function ok() {
+
+      }, function err(e) {
+        $scope.setError(e.data.message);
+      })
       .finally(function () {
         $scope.working--;
       });
