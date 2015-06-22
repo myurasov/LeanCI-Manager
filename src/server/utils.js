c8a60ef6b520d828066707a983d8f2889a1b6f27
Mikhail Yurasov <me@yurasov.me>
2015-06-22 07:13:11 -0700

    Comments

--- src/server/utils.js
+++ src/server/utils.js
@@ -6,12 +6,19 @@
 
 var services = {};
 
+/**
+ * Define service
+ */
 exports.defineService = function (app, name, factory) {
+
+  // store constructor into app
   app.set(name, function () {
+    // make sure factory method is called only once
     if (!services[name]) {
       services[name] = factory();
     }
     return services[name];
   });
+
 };
 
