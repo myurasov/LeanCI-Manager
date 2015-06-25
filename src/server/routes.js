deff340154040c9ef4ecd300641ff1390d13c59c
Mikhail Yurasov <me@yurasov.me>
2015-06-25 03:56:21 -0700

    Remove Settings API

--- src/server/routes.js
+++ src/server/routes.js
@@ -7,7 +7,6 @@
 module.exports = function (app) {
 
   app.use('/', require('./routes/index'));
-  app.use('/api/settings', require('./routes/api/settings'));
   app.use('/api/users', require('./routes/api/users'));
 
 };
