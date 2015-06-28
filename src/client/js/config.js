52c19589c19c9eebca9dfc1f04b843f4f22b3b2c
Mikhail Yurasov <me@yurasov.me>
2015-06-27 23:04:03 -0700

    Confugure icon paths

--- src/client/js/config.js
+++ src/client/js/config.js
@@ -5,10 +5,20 @@
 'use strict';
 
 // angular material theming
-app.config(function ($mdThemingProvider) {
+app.config(function ($mdThemingProvider, $mdIconProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('blue')
     .accentPalette('yellow');
+
+  $mdIconProvider
+    .defaultIconSet("vendor /assets/svg/avatars.svg", 128)
+    .icon("menu", "./assets/svg/menu.svg", 24)
+    .icon("share", "./assets/svg/share.svg", 24)
+    .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+    .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+    .icon("twitter", "./assets/svg/twitter.svg", 512)1
+    .icon("phone", "./assets/svg/phone.svg", 512);
+
 });
 
 // api endpoint
